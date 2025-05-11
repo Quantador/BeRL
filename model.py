87bc6396b90d3e1b8ba7cf483903955af5a7cb54
CHOOSEIT <chooseitytt@gmail.com>
2025-05-11 14:14:30 +0200

    add code

---
+++ model.py
@@ -0,0 +1,26 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.distributions import Categorical
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+class Policy(nn.Module):
+    def __init__(self, state_size=4, action_size=2, hidden_size=32):
+        super(Policy, self).__init__()
+        self.fc1 = nn.Linear(state_size, hidden_size)
+        self.fc2 = nn.Linear(hidden_size, action_size)
+
+    def forward(self, state):
+        x = F.relu(self.fc1(state))
+        x = self.fc2(x)
+        # we just consider 1 dimensional probability of action
+        return F.softmax(x, dim=1)
+
+    def act(self, state):
+        state = torch.from_numpy(state).float().unsqueeze(0).to(device)
+        probs = self.forward(state).cpu()
+        model = Categorical(probs)
+        action = model.sample()
+        return action.item(), model.log_prob(action)
